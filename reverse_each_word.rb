a1853c95dae0e83fbd20e4d526ff233001ad5bb8
Amber Taranto <ambzzzz@gmail.com>
2019-06-05 22:34:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(string)
     array = string.split(" ")
-    # reversed_array = array.collect { |word| word.reverse }
-    # reversed_array.join(" ")
-
-    array.collect! { |word| word.reverse }
-    array.join " "
+    reversed_array = array.collect { |word| word.reverse }
+    reversed_array.join(" ")
 end
